6df5dbf8d3a2945408934f3dda9240ba2dcb08a6
yasser100ali <108766553+yasser100ali@users.noreply.github.com>
2025-10-16 15:50:06 -0700

    fixed typo in config, testing on prod now

--- next.config.js
+++ next.config.js
@@ -7,21 +7,21 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : `${process.env.RAILWAY_BACKEND_URL || "dataanalyst-production-2e27.up.railway.app"}/api/:path*`,
+            : `${process.env.RAILWAY_BACKEND_URL || "https://dataanalyst-production-2e27.up.railway.app"}/api/:path*`,
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
-            : `${process.env.RAILWAY_BACKEND_URL || "dataanalyst-production-2e27.up.railway.app"}/docs`,
+            : `${process.env.RAILWAY_BACKEND_URL || "https://dataanalyst-production-2e27.up.railway.app"}/docs`,
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/openapi.json"
-            : `${process.env.RAILWAY_BACKEND_URL || "dataanalyst-production-2e27.up.railway.app"}/openapi.json`,
+            : `${process.env.RAILWAY_BACKEND_URL || "https://dataanalyst-production-2e27.up.railway.app"}/openapi.json`,
       },
     ];
   },
