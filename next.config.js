45d0cd24c910651dbf605ce5f5340e64bff3bae7
yasser100ali <108766553+yasser100ali@users.noreply.github.com>
2025-10-16 15:46:17 -0700

    hooked up backend to railway

--- next.config.js
+++ next.config.js
@@ -7,21 +7,21 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+            : `${process.env.RAILWAY_BACKEND_URL || "dataanalyst-production-2e27.up.railway.app"}/api/:path*`,
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
+            : `${process.env.RAILWAY_BACKEND_URL || "dataanalyst-production-2e27.up.railway.app"}/docs`,
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
+            : `${process.env.RAILWAY_BACKEND_URL || "dataanalyst-production-2e27.up.railway.app"}/openapi.json`,
       },
     ];
   },
